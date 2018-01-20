495eb9529ee86fb196cdec7cbf4f8f70bf366aff
riteshsharma29 <riteshsharma29@gmail.com>
2018-01-20 19:31:37 +0530

    Add files via upload

---
+++ categorytable.sql
@@ -0,0 +1,8 @@
+﻿TRUNCATE TABLE categorytable;
+INSERT INTO `categorytable` VALUES (1, -1, 1, '', 0, 0, 0, 0, 0, '', '', 1, 0, 1, 1, 7, '');
+INSERT INTO `categorytable` VALUES (2,1,1,'',1,0,0,1,1,501,'', 1, 0, 1, 1, 7, '');
+INSERT INTO `categorytable` VALUES (3,1,2,'',2,0,0,2,2,502,'', 1, 0, 1, 1, 7, '');
+INSERT INTO `categorytable` VALUES (4,1,3,'',3,0,0,3,3,503,'', 1, 0, 1, 1, 7, '');
+INSERT INTO `categorytable` VALUES (5,1,4,'',4,0,0,4,4,504,'', 1, 0, 1, 1, 7, '');
+INSERT INTO `categorytable` VALUES (6,1,5,'',5,0,0,5,5,505,'', 1, 0, 1, 1, 7, '');
+INSERT INTO `categorytable` VALUES (7,1,6,'',6,0,0,6,6,506,'', 1, 0, 1, 1, 7, '');
---
+++ linktable.sql
@@ -0,0 +1,7 @@
+﻿TRUNCATE TABLE linktable;
+INSERT INTO `linktable` VALUES (2,101,'', 0, '', '', 1, 1, 7);
+INSERT INTO `linktable` VALUES (3,102,'', 0, '', '', 1, 1, 7);
+INSERT INTO `linktable` VALUES (4,103,'', 0, '', '', 1, 1, 7);
+INSERT INTO `linktable` VALUES (5,104,'', 0, '', '', 1, 1, 7);
+INSERT INTO `linktable` VALUES (6,105,'', 0, '', '', 1, 1, 7);
+INSERT INTO `linktable` VALUES (7,106,'', 0, '', '', 1, 1, 7);
